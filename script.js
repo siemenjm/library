e98e48a4a726dce4361b67e8f55abe4324ffb3f9
Jared Siemen <siemenjm@gmail.com>
2020-10-20 14:12:20 -0400

    Add form.

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let myLibrary = [];
+//Global Variables--------------------------------------------------
+//------------------------------------------------------------------
 
-function Book(title, author, pages, read, rating = "N/A") {
+let myLibrary = [];
+const table = document.querySelector("#book-table");
+//const addBtn = document.querySelector("#add-btn");
+
+//Event Listeners---------------------------------------------------
+//------------------------------------------------------------------
+
+//addBtn.addEventListener("click", function() {update()});
+
+//Book Constructor--------------------------------------------------
+//------------------------------------------------------------------
+
+function Book(title, author, genre, pages, read, rating = "N/A") {
     this.title = title;
     this.author = author;
+    this.genre = genre;
     this.pages = pages;
     this.read = read;
     this.rating = rating;
@@ -11,14 +25,16 @@ function Book(title, author, pages, read, rating = "N/A") {
     }
 }
 
-function addBookToLibrary(title, author, pages, read, rating) {
-    let book = new Book(title, author, pages, read, rating);
+//Helper Functions--------------------------------------------------
+//------------------------------------------------------------------
+
+function addBookToLibrary(title, author, genre, pages, read, rating) {
+    let book = new Book(title, author, genre, pages, read, rating);
     
     myLibrary.push(book);
 }
 
 function displayBooks(libraryArray) {
-    let table = document.querySelector("#book-table");
     while (table.rows.length > 1) {
         table.deleteRow(-1);
     }
@@ -26,20 +42,21 @@ function displayBooks(libraryArray) {
         let row = table.insertRow(-1);
         row.insertCell(0).textContent = book.title;
         row.insertCell(1).textContent = book.author;
-        row.insertCell(2).textContent = book.pages;
-        row.insertCell(3).textContent = book.read;
+        row.insertCell(2).textContent = book.genre;
+        row.insertCell(3).textContent = book.pages;
+        row.insertCell(5).textContent = book.read;
         row.insertCell(4).textContent = book.rating;
     });
 }
 
+//Main Function-----------------------------------------------------
+//------------------------------------------------------------------
 
-//run
-addBookToLibrary("The Silmarillion","J.R.R. Tolkien", 197, "have read", 5);
-addBookToLibrary("Harry Potter","J.K. Rowling", 528, "have read", 4);
-displayBooks(myLibrary);
+function update() {
+    addBookToLibrary("The Silmarillion","J.R.R. Tolkien", "Fantasy", 197, "have read", 5);
+    addBookToLibrary("Harry Potter","J.K. Rowling", "Children's Fantasy", 528, "have read", 4);
+    addBookToLibrary("Harry Potter 2","J.K. Rowling", "Children's Fantasy",423, "have not read");
+    addBookToLibrary("Harry Potter 3","J.K. Rowling", "Children's Fantasy",65, "have not read");
 
-addBookToLibrary("Harry Potter 2","J.K. Rowling", 423, "have not read");
-displayBooks(myLibrary);
-
-addBookToLibrary("Harry Potter 3","J.K. Rowling", 65, "have not read");
-displayBooks(myLibrary);
+    displayBooks(myLibrary);
+}
